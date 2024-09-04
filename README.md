8fe4be293ca5a9e76ce77c7366011db2ee152102
RicardoRibeiroRodrigues <riywar@gmail.com>
2024-09-04 15:00:32 -0300

    Add all package logic

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample package for the Open Dev course on INSPER
+
+More about the course at https://insper.github.io/open-dev/
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# which argument is for supported operating systems?
+setup(
+    name="dev_aberto_ricardorr",
+    version="0.1",
+    packages=["dev_aberto"],
+    author="Ricardo Rodrigues",
+    platforms=["Linux", "MacOS", "Windows"],
+    install_requires=["requests"],
+    scripts=["scripts/hello.py"],
+)
